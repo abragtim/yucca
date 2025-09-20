7badc5b56cf16410a4ffb59014ce22fd69a24761
Timur Abragimovich <timur.abragimovich@mild.blue>
2025-09-20 17:55:58 +0200

    Add SSIM loss. (#2)

--- yucca/modules/optimization/loss_functions/SSIM.py
+++ yucca/modules/optimization/loss_functions/SSIM.py
@@ -1,9 +1,10 @@
-from torch import nn, Tensor
+from torch import Tensor
+from kornia.losses import SSIMLoss
 
 
-class SSIM(nn.SSIMLoss):
+class SSIM(SSIMLoss):
     def __init__(self):
-        super().__init__()
+        super().__init__(window_size=11)
 
     def forward(self, input: Tensor, target: Tensor) -> Tensor:
         return super().forward(input, target)
